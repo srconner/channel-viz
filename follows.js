6d4fb87dd9fa72c7eddb369b3796cff7524d9052
srconner <srconner@yahoo.com>
2015-03-17 11:43:05 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,8 +16,8 @@
 		applicationName	= 'Steve\'s Smoker Controller', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor	= '', // CSS HEX value of color to represent data (omit leading #)
+		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
